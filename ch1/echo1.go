ff73ecbc7f2cac7e04d013f689133f8bb006f79e
MIIIG <arthur.badretdinov@gmail.com>
2022-09-07 13:33:42 +0500

    add practic ch1.1 ch1.2 ch1.3

--- ch1/echo1.go
+++ ch1/echo1.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	var s, sep string
-	for i :=1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
-		sep = " "
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + strconv.Itoa(i) + ": " + os.Args[i]
+		sep = "\n"
 	}
 	fmt.Println(s)
 }
